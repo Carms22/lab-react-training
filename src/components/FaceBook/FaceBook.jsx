699556aa86b8e0a67c7fedd7437d14936f014945
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-09-26 17:27:31 +0200

    Add 14th

--- src/components/FaceBook/FaceBook.jsx
+++ src/components/FaceBook/FaceBook.jsx
@@ -1,39 +1,51 @@
 import profiles from '../../data/berlin.json'
 import React from "react";
-
+import './FaceBook.css'
+const coutries = Array.from(
+  new Set(profiles.map(profile => profile.country))
+)
 
 class FaceBook extends React.Component {
   state = {
-    color: "white"
+    country: null,
   }
-  myCountry = () =>{
-    return{
-      
-    }
+  onCountryClick = (event) =>{
+    const value = event.target.value
+    this.setState({ country: value || null})
   }
 
   render (){
+    const { country: countryState } = this.state
+    const profileToRender =countryState ? profiles.filter(profile => profile.country === countryState ) : profiles
+
     return (
-      <div>
-        <button onClick={this.myCountry} className='Button'>USA</button>
-        <button className='Button'>England</button>
-        <button className='Button'>Malaysia</button>
-        <button className='Button'>Germany</button>
-        <button className='Button'>Sweden</button>
+      <div className='Facebook'>
+        <button className={!countryState ? 'selected' : '' }>All</button>
         {
-          profiles.map( (profile,i) => {
-            const backGColor= profile.isStudent ? "blue" :  "pink"
-            return (
-            <div style={{backgroundColor: backGColor}} key={i}>
-              <img src={profile.img} alt="pic"/>
+          coutries.map( (country,i) => (
+           <button
+            value={country} key={i} onClick={this.onCountryClick}
+            className={ countryState === country ? 'selected' : ''}
+           >
+            {country}
+           </button> 
+          
+          ))
+        }
+        {
+          profileToRender.map( (profile,i) => (
+            <div key={i} className={`Facebook__item ${profile.country === countryState ? 'selected' : ''}` }>
               <div>
-                <h2>FirstName: {profile.firstName}</h2>
-                <h2>LastName: {profile.lastName}</h2>
-                <h3>Country: {profile.country}</h3>
+                <img src={profile.img} alt="pic"/>
+                <div>
+                  <h2><strong>FirstName:</strong> {profile.firstName}</h2>
+                  <h2><strong>LastName:</strong> {profile.lastName}</h2>
+                  <h3><strong>Country:</strong> {profile.country}</h3>
+                  <h3><strong>Tupy:</strong> {profile.isStudent ? 'Student' : 'Teaches'}</h3>
+                </div>
               </div>
             </div>
-            )
-          })
+          ))
         }
       </div>
     )
--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -1,34 +1,32 @@
 import React, { Component } from 'react'
 import './LikeButton.css'
+const colors = ['purple','blue','green','yellow','orange','red'];
 
 class LikeButton extends Component {
   state = {
     count: 0 ,
-    color: ['purple','blue','green','yellow','orange','red'],
     colorChoose: ''
   }
 
-  randomColor = (color) => {
-    console.log(color) 
-    const index = Math.floor(Math.random()*color.length)
-    return { 
-      colorChoose : color[index]
-    }
-  }
+
   add = () => {
-    this.randomColor (this.state.color)
-    console.log( this.randomColor (this.state.color));
-    this.setState({ count: this.state.count + 1 }, () => {
+    this.setState(prevState =>{ 
+      return {count:prevState.count + 1 
+      }
     }) 
-   
   }
 
   render() {
-    console.log('render')
-    const { count, colorChoose } = this.state
+  
+    const { count } = this.state
     return (
       <div>
-        <button className="Button" onClick={this.add} style ={{backgroundColor: colorChoose}}>{count} Likes</button>
+        <button 
+          className="Button" onClick={this.add} 
+          style ={{backgroundColor: colors[ count % colors.length] }}
+        >
+          {count} {count === 1 ? "like" : "likes"}
+        </button>
       </div>
     )
   }
